3bc0141cb6e528f0d8ca37463a63074961cec96f
Your Name <mahinmittal2018@gmail.com>
2022-03-14 13:17:08 +0530

    basic CRUD

---
+++ index.js
@@ -0,0 +1,42 @@
+const debug=require('debug')('app:startup');
+const config=require('config');
+const Joi=require('joi');
+const helmet=require('helmet');
+const morgan=require('morgan');
+const express = require('express');
+const req = require('express/lib/request');
+const cources=require('./routes/cources')
+const home=require('./routes/home');
+const logger =require('./middleware/logger')
+const app =express();
+
+app.set('view engine','pug');
+app.set('views','./views');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.use(helmet());
+app.use('/api/cources',cources)
+app.use('/',home)
+
+//configruation
+console.log('Application name : '+config.get('name'));
+console.log('Mail server : '+config.get('mail.host'));
+// console.log('Mail password : '+config.get('mail.password'));
+
+if (app.get('env')==='development'){
+    app.use(morgan('tiny'));
+    debug('Morgan enabled.....');
+}
+
+
+
+
+app.use(function log(req,res,next){
+    console.log('Logging.......');
+      next();
+    });
+
+//port
+ const port = process.env.PORT || 3000;
+ app.listen(port,()=> console.log(`Listening on port ${port}...`));
---
+++ routes/cources.js
@@ -0,0 +1,70 @@
+const express=require('express');
+const router=express.Router();
+const Joi=require('joi');
+
+const cources =[
+    {id: 1,name:'cource1'},
+    {id: 2,name:'cource2'},
+    {id: 3,name:'cource3'},
+]
+
+
+
+router.get('/',(req,res)=>{
+    res.send(cources);
+})
+
+router.get('/:id', (req, res) => {
+    const cource = cources.find(c=>c.id === parseInt(req.params.id));
+    if (!cource) return res.status(404).send('the cource with given id not avalible.');
+     else return res.send(cource);
+ });
+
+ router.post('/', (req, res) => {
+     const {error}=ValidateCource(req.body);
+     if(error){
+         res.status(400),send(result.error.details[0].message);
+         return;
+     }
+    const cource={
+          id: cources.length +1,
+          name:req.body.name
+      };
+     cources.push(cource);
+     res.send(cource);
+      });
+
+
+
+router.put('/:id',(req,res)=>{
+     const cource=cources.find(c=>c.id === parseInt(req.params.id));
+     if(!cource) return res.status(404).send('The cource with given Id was not avalible');
+     const {error} =ValidateCource(req.body);
+     if(error){
+       return  res.status(400).send(error.details[0].message);
+
+     }
+ cource.name=req.body.name;
+ res.send(cource);
+ });
+
+router.delete('/:id',(req,res)=>{
+     const cource=cources.find(c=> c.id === parseInt(req.params.id));
+     if(!cource){
+
+      return res.status(404).send('The cource with given Id was not avalible');
+     }
+     const index =cources.indexOf(cource);
+    cources.splice(index,1);
+
+     res.send(cources);
+});
+ 
+function ValidateCource(cource){
+    const schema=
+         Joi.object({name:Joi.string().min(3).required()});
+    
+     return  schema.validate(cource);
+}
+
+module.exports=router;
