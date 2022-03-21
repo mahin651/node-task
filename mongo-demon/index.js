565db5a23f0a9340b8ae96d015476a57acacdba9
Your Name <mahinmittal2018@gmail.com>
2022-03-21 17:16:48 +0530

    validators

--- mongo-demon/index.js
+++ mongo-demon/index.js
@@ -1,4 +1,5 @@
 
+const number = require('joi/lib/types/number');
 const mongoose=require('mongoose');
 
 mongoose.connect('mongodb+srv://new:new651@cluster0.ivduc.mongodb.net/nodeapp?retryWrites=true&w=majority')
@@ -6,34 +7,69 @@ mongoose.connect('mongodb+srv://new:new651@cluster0.ivduc.mongodb.net/nodeapp?re
 .catch(err => console.error('Could not connect to db',err));
 
 const courceSchema=new mongoose.Schema({
-    name:{type:String,required:true},
+    name:{type:String,
+        required:true,
+        minlength:5,
+        maxlength:255
+    },
+    category:{
+        type:String,
+        lowercase:true,
+        required:true,
+        trim:true,  
+        enum:['web','mobile','network']
+    },
     author:String,
-    tags:[String],
+    
+    tags:{
+        type:Array,
+        validate:{
+       isAsync:true,
+        validator:function(v,callback){
+            setTimeout(() => {
+                const result=v && v.lenght>0;
+                callback(result);
+            },4000);
+        },
+        message:'A cource should have atleast one tag.'
+        }
+    },
     date:{type:Date,default:Date.now},
-    isPublished:Boolean
+    isPublished:Boolean,
+    price:{
+        type : Number,
+        required:function() {return this.isPublished;},
+        min:10,
+        max:200,
+        get: v => Math.round(v),
+        set: v => Math.round(v)
+    
+    }
 });
 
 const Cource= mongoose.model('Cource',courceSchema);
 async function createCource(){
 const  cource = new Cource({
-// name:'Angular cource',
+ name:'Angular cource',
+ category:'web',
 author:'mosh',
 tags:['angular','frontend'],
-isPublished:true
+isPublished:true,
+price:15
 });
 
 try{
-    const isValid =await cource.validate((err)=>{
-        if(err){}
-    })
+    
  const result= await cource.save();
  console.log(result);
 }
-catch{
-    console.log(ex.message);
+catch(ex){
+    for(field in ex.errors)
+    console.log(ex.errors[field].message);
 }
 }
-
+createCource()
+ 
 async function getCource(){
 const pageNumber =2;
 const pageSize =10;
@@ -43,7 +79,7 @@ const cources = await Cource.find({author:'mosh'})
 .sort({name:1})
 .skip((pageNumber -1) * pageSize)
 .limit(pageSize)
-.select({name:1,tags:1})
+.select({name:1,tags:1,price:1})
 console.log(cources);
 }
 getCource();
@@ -61,5 +97,6 @@ async function updateCource(id){
     // })
 }
 updateCource('6232e3b66570965012486d66');
+
 
 
